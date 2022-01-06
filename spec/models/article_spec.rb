77a6b6d7b33acc5a813247d55ae9580f2d6526b4
Waihon Yew <yewwaihon@gmail.com>
2022-01-06 22:34:02 +0800

    Test that factory is valid

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests article object" do
-    article = create(:article)
+  it "tests that factory is valid" do
+    article = build(:article)
 
-    expect(article.title).to eq("Sample article")
-    expect(article.new_record?).to be_falsey
+    expect(article).to be_valid
   end
 end
