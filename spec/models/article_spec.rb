3ee97348c9e9338753ccdddef0340f7c4d4a5b40
Waihon Yew <yewwaihon@gmail.com>
2022-01-06 22:56:48 +0800

    Make the test examples DRY by using let to define a memoized helper method

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe "#validations" do
-    it "tests that factory is valid" do
-      article = build(:article)
+    let(:article) { build(:article) }
 
+    it "tests that factory is valid" do
       expect(article).to be_valid
     end
 
     it "has an invalid title" do
-      article = build(:article, title: "")
+      article.title = ""
 
       expect(article).not_to be_valid
       expect(article.errors[:title]).to include("can't be blank")
