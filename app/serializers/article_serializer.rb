f84536ef96ee2e2aa426c08ef05d30c8fc028135
Waihon Yew <yewwaihon@gmail.com>
2022-01-08 17:05:25 +0800

    Use set_type to change the type to plural form which is typical for JSON:API

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,4 +1,11 @@
 class ArticleSerializer
   include JSONAPI::Serializer
+
+  # By default jsonapi_serializer will try to figure the type based on the
+  # name of the serializer class. For example class MovieSerializer will
+  # automatically have a type of :movie. If your serializer class name does not
+  # follow this format, you have to manually state the set_type at the
+  # serializer.
+  set_type :articles
   attributes :title, :content, :slug
 end
