4432f9a468d0ca2f4acbd454e3829271a82ba67e
Waihon Yew <yewwaihon@gmail.com>
2022-01-08 17:53:28 +0800

    Auto require and include API helper methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -64,6 +64,9 @@ RSpec.configure do |config|
 
   # Add FactoryBot methods
   config.include FactoryBot::Syntax::Methods
+
+  # Include custom API helper methods
+  config.include ApiHelpers
 
   # Start by truncating all the tables but then use the faster transaction
   # strategy test rest of the times.
