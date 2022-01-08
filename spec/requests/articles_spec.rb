1c64315d0430ae3da5ede9bdf835dd7ac3d935d8
Waihon Yew <yewwaihon@gmail.com>
2022-01-08 15:17:55 +0800

    Test get /articles to return a proper JSON

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe ArticlesController do
       # expect(response).to have_http_status(200)
       expect(response).to have_http_status(:ok)
     end
+
+    it "returns a proper JSON" do
+      article = create(:article)
+      get "/articles"
+      body = JSON.parse(response.body)
+      expect(body).to eq(
+        data: [
+          {
+            id: article.id,
+            type: "articles",
+            attributes: {
+              title: article.title,
+              content: article.content,
+              slug: article.slug
+            }
+          }
+        ]
+      )
+    end
   end
 end
