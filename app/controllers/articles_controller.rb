502af12521ed9f0a269dcfeb26d97372d6afe9dc
Waihon Yew <yewwaihon@gmail.com>
2022-01-09 15:42:15 +0800

    Use the simplified pagination interface

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,13 @@
 class ArticlesController < ApplicationController
+  include Paginable
+
   def index
-    articles = Article.recent
-    paginated = paginator.call(
-      articles,
-      params: pagination_params,
-      base_url: request.url
-    )
+    paginated = paginate(Article.recent)
     options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
     render json: serializer.new(paginated.items, options), status: :ok
   end
 
   def serializer
     ArticleSerializer
-  end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-
-  def pagination_params
-    params.permit![:page]
   end
 end
