691941e4a654e76653306845c6743a4450b72b86
Waihon Yew <yewwaihon@gmail.com>
2022-01-09 13:28:30 +0800

    Test pagination of articles resource

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -34,5 +34,21 @@ RSpec.describe ArticlesController do
       ids = json_data.map { |item| item[:id].to_i }
       expect(ids).to eq([recent_article.id, older_article.id])
     end
+
+    it "paginates results" do
+      article1, article2, article3 = create_list(:article, 3)
+      get "/articles", params: { page: { number: 2, size: 1 } }
+      expect(json_data.length).to eq(1) # size
+      expect(json_data.first[:id]).to eq(article2.id)
+    end
+
+    it "contains pagination links in the response" do
+      article1, article2, article3 = create_list(:article, 3)
+      get "/articles", params: { page: { number: 2, size: 1 } }
+      expect(json[:links].length).to eq(5)
+      expect(json[:links].keys).to contain_exactly(
+        "first", "prev", "next", "last", "self"
+      )
+    end
   end
 end
