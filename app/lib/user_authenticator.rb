b300b0a19f895c6a1a3f94733f1423f766967a2a
Waihon Yew <yewwaihon@gmail.com>
2022-01-12 08:28:19 +0800

    Mock Oktokit's response and test UserAuthenticator when valid code is provided

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -4,10 +4,23 @@ class UserAuthenticator
   attr_reader :user
 
   def initialize(code)
-
+    @code = code
   end
 
   def perform
-    raise AuthenticationError
+    client = Octokit::Client.new(
+      client_id: ENV["GITHUB_CLIENT_ID"],
+      client_secret: ENV["GITHUB_CLIENT_SECRET"]
+    )
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present?
+      raise AuthenticationError
+    else
+      # user registration
+    end
   end
+
+private
+
+  attr_reader :code
 end
